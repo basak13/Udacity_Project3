c761e0c4b02fcf81db21ff8bedc5fc9ea8f1b11f
basak13 <73781898+basak13@users.noreply.github.com>
2020-11-04 17:19:55 +0300

    Update design.js

--- design.js
+++ design.js
@@ -1,31 +1,34 @@
+//Defining Event for submit. When size is submitted by the user, call makeGrid()
 var sizePicker = document.getElementById("sizePicker");
 var table = document.getElementById("pixelCanvas");
 
-sizePicker.addEventListener('submit' ,function(event) {
-           event.preventDefault();
-           makeGrid();
-       })
+sizePicker.addEventListener('submit', function(event) {
+  event.preventDefault();
+  makeGrid();
+})
+//Defining makeGrid function
 
 function makeGrid() {
 
-var width = document.getElementById("inputWidth").value;
-var height = document.getElementById("inputHeight").value;
+  var width = document.getElementById("inputWidth").value;
+  var height = document.getElementById("inputHeight").value;
 
   table.innerHTML = "";
 
-    for (var y = 0; y < height; y++) {
-       var row = table.insertRow(y);
-       for (var x = 0; x < width; x++) {
-             var cell = row.insertCell(x);
-             cell.onclick = function () {
-            paint(this);
-        };
-           }
-        }
+  // Building up  Grid.
+  for (var y = 0; y < height; y++) {
+    var row = table.insertRow(y);
+    for (var x = 0; x < width; x++) {
+      var cell = row.insertCell(x);
+      cell.onclick = function() {
+        paint(this);
+      };
     }
-
-
-  function paint(cell) {
-  var color = document.getElementById("colorPicker").value;
-   cell.style.backgroundColor = color;
   }
+}
+
+//Creating function to color cells
+function paint(cell) {
+  var color = document.getElementById("colorPicker").value;
+  cell.style.backgroundColor = color;
+}
